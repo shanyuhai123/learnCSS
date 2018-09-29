f161d6ed3b71ed005efe6253e248a011e609f9f2
yangxinyuan <864299347@qq.com>
2018-09-29 21:50:46 +0800

    Lights with π digits

---
+++ 0115-lights-with-pi-digits/index.js
@@ -0,0 +1,22 @@
+const PI =
+  "3141592653589793238462643383279502884197169399375105820974944592307816406286208998628034825342117067";
+let number = 1;
+
+d3.select('.pi')
+  .selectAll('span')
+  .data(PI)
+  .enter()
+  .append('span')
+  .attr('class', (d) => `d${d}`)
+  .style('--d', (d) => d)
+  .text((d) => d);
+
+function show() {
+  d3.selectAll(`.pi span.d${number % 10}`)
+    .classed("show", true);
+  d3.selectAll(`.pi span.d${(number - 1) % 10}`)
+    .classed("show", false);
+  number++;
+}
+
+setInterval(show, 500);
