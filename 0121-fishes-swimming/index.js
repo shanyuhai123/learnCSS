675904dc85137f9898875751862cf18a4d8a2469
shanyuhai123 <864299347@qq.com>
2018-10-03 23:17:20 +0800

    Fishes swimming

---
+++ 0121-fishes-swimming/index.js
@@ -0,0 +1,26 @@
+function buildFish(e) {
+  let fish = d3.select('body')
+      .append('div')
+      .attr('class', 'fish')
+      .style('--size', d3.randomUniform(5, 8)())
+      .style('--color', d3.randomUniform(-60, 15)())
+      .style('--duration', d3.randomUniform(3, 6)())
+      .style('--top', e.clientY ? e.clientY : d3.randomUniform(100, window.innerHeight)());
+  
+  fish.append('span').attr('class', 'body');
+  fish.append('span').attr('class', 'eye');
+  fish.append('span').attr('class', 'fin');
+  fish.append('span').attr('class', 'tail');
+}
+
+function buildRipple(e) {
+  d3.select('body')
+    .append('div')
+    .attr('class', 'ripple')
+    .style('--left', e.clientX)
+    .style('--top', e.clientY);
+};
+
+window.addEventListener('click', buildFish);
+window.addEventListener("click", buildRipple);
+d3.range(3).forEach(buildFish);
