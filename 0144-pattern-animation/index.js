379ff6988827806231dbd1ea80e7f2c75234c6b3
yangxinyuan <864299347@qq.com>
2018-10-09 21:07:56 +0800

    Pattern animation

---
+++ 0144-pattern-animation/index.js
@@ -0,0 +1,37 @@
+const SIDE_LENGTH = 20;
+let animation = new TimelineMax({ repeat: -1 });
+let $horizontalSpan = '.container .horizontal span';
+let $verticalSpan = '.container .vertical span';
+
+let container = d3.select('.container').style('--side-length', SIDE_LENGTH);
+
+function appendSpan(selector) {
+  container
+    .select(selector)
+    .selectAll('span')
+    .data(d3.range(SIDE_LENGTH * SIDE_LENGTH))
+    .enter()
+    .append('span');
+}
+
+appendSpan('.horizontal');
+appendSpan('.vertical');
+
+animation
+  .to($horizontalSpan, 1, { rotation: 45 }, 'step1')
+  .to($horizontalSpan, 1, { x: '-10px', y: '-10px' }, 'step2')
+  .to(
+    $horizontalSpan,
+    1,
+    { rotation: 0, x: '0', y: '0', scaleY: 2, scaleX: 0.5 },
+    'step3'
+  )
+  .to($horizontalSpan, 1, { rotation: 90, scaleY: 1, scaleX: 1 }, 'step4');
+
+animation
+  .to($verticalSpan, 1, { rotation: 45 }, 'step1')
+  .to($verticalSpan, 1, { x: '10px', y: '10px' }, 'step2')
+  .to($verticalSpan, 1, { x: '0', y: '0', scaleX: 2, scaleY: 0.5 }, 'step3')
+  .to($verticalSpan, 1, { rotation: 90, scaleX: 1, scaleY: 1 }, 'step4');
+
+animation.timeScale(2);
